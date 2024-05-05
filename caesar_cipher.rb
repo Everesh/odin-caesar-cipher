1841e6ef50add6bd342d97ffbfd018d023437cc3
Everesh <everesh@proton.me>
2024-05-05 03:34:11 +0200

    Find int values of upper & lower case alphabet

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,5 +1,13 @@
 def caesar_cipher(str, num)
+    #int value of A = 65
+    #int value of Z = 90
+    #int value of a = 97
+    #int value of z = 122
 
-    # TO DO
 
 end
+
+puts "a".codepoints
+puts "z".codepoints
+puts "A".codepoints
+puts "Z".codepoints
