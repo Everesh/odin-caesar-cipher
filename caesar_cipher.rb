fb13306256962947ee35dea3083f06e91b3bd310
Everesh <everesh@proton.me>
2024-05-05 21:39:00 +0200

    Fix formating via rubocop

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,16 +1,14 @@
+# frozen_string_literal: true
+
 def caesar_cipher(str, num)
-    #int value of A = 65
-    #int value of Z = 90
-    #int value of a = 97
-    #int value of z = 122
-
-    str.codepoints.map do |value|
-        if (value >= 65 && value <=90)
-            value = (value - 65 + num) % 26 + 65
-        elsif value >=97 && value <=122
-            value = (value - 97 + num) % 26 + 97
-        end
-        value.chr
-    end.join
-
+  # int values of uppercase alphabet A..Z = 65..90
+  # int values of lowercase alphabet a..z = 97..122
+  str.codepoints.map do |value|
+    if value >= 65 && value <= 90
+      value = (value - 65 + num) % 26 + 65
+    elsif value >= 97 && value <= 122
+      value = (value - 97 + num) % 26 + 97
+    end
+    value.chr
+  end.join
 end
